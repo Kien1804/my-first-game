d12727cb0f6c7b2f8cf1cd647d6c4be0ecae0d6e
Kien1804 <cyeng1804@gmail.com>
2024-06-28 23:51:35 +0700

    day 2

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -2,15 +2,18 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame with HasKeyboardHandlerComponents{
   @override
   Color backgroundColor() => const Color(0xFF211F30); //xóa Blackbar ở 2 bên
   late final CameraComponent cam;
 
-  final world = Level();
+  final world = Level(
+    levelName: 'Level-01'
+  );
 
   @override
   FutureOr<void> onLoad() async{
